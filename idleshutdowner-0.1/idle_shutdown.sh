4c720dd3380bce4ce798516aae7e212bd25a4abe
Ruben Cancho <canchete@gmail.com>
2021-06-17 18:30:18 +0200

    Fixed bug

--- idleshutdowner-0.1/idle_shutdown.sh
+++ idleshutdowner-0.1/idle_shutdown.sh
@@ -1,22 +1,18 @@
 #!/bin/sh
 
-idle_time=`expr $1 \* 60 \* 60`
+idle_time=`expr $1`
 
 if [ -z "$idle_time" ]; then
   exit 1
 fi
 
-get_terminal_idle_seconds() {
-  expr `date +%s` - `stat --format %X /dev/pts/* | sort -n | tail -1`
-}
-
-x_server_accessible() {
-  DISPLAY=:0 xprintidle > /dev/null
+x_xprintidle_ok() {
+  xprintidle > /dev/null
   return $?
 }
 
 get_x_idle_seconds() {
-  expr `DISPLAY=:0 xprintidle` / 1000
+  expr `xprintidle` / 1000
 }
 
 shutdown_system() {
@@ -25,17 +21,13 @@ shutdown_system() {
 }
 
 while true; do
-  terminal_idle_seconds=`get_terminal_idle_seconds`
-  if [ "$terminal_idle_seconds" -gt "$idle_time" ]; then
-    if x_server_accessible; then
-      x_idle_seconds=`get_x_idle_seconds`
-      if [ "$x_idle_seconds" -gt "$idle_time" ]; then
-        logger shutting down due to idle timer
-        shutdown_system
-      fi
-    else
+  x_idle_seconds=`get_x_idle_seconds`
+  if [[ x_xprintidle_ok ]] ; then
+    x_idle_seconds=`get_x_idle_seconds`
+    logger "X Idle seconds: $x_idle_seconds"
+    if [ "$x_idle_seconds" -gt "$idle_time" ]; then
       logger shutting down due to idle timer
-      shutdown_system
+      #shutdown_system
     fi
   fi
   sleep 60
